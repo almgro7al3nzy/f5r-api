e0ad6a130f7ef94fd2008336261b81657bd3b106
Abbas Aleinzy | عباس العنزي <42795041+almgro7al3nzy@users.noreply.github.com>
2022-09-27 09:12:02 +0300

    ss

--- Models/Dbcontext.js
+++
@@ -1,8 +0,0 @@
-const mongoose  =  require('mongoose');
-require('./UserModel')
-mongoose.connect("mongodb://127.0.0.1:27017/Plebbit",
-{useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex :true}).then((result) => {
-    console.log("Success")
-}).catch((err) => {
-    console.log(err)
-});
---
+++ src/app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const userRouter = require("./routes/userRoutes");
+const roomRouter = require("./routes/roomRoutes");
+const messageRouter = require("./routes/messageRoutes");
+const helmet = require("helmet");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const cookieParser = require("cookie-parser");
+const compression = require("compression");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controller/errorController");
+
+const app = express();
+app.use(cors());
+
+app.options("*", cors());
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Limit requests from same API
+// const limiter = rateLimit({
+//   max: 100,
+//   windowMs: 60 * 60 * 1000,
+//   message: "Too many requests from this IP, please try again in an hour!",
+// });
+// app.use("/api", limiter);
+
+//Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+app.use(compression());
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+//Routes
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/room", roomRouter);
+app.use("/api/v1/messages", messageRouter);
+
+// app.all("*", (req, res, next) => {
+//   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+// });
+
+app.use(globalErrorHandler);
+
+module.exports = app;
