9e6bb3e1b66dddc1383c32555fc956dad9ae74a0
Abbas Aleinzy | عباس العنزي <42795041+almgro7al3nzy@users.noreply.github.com>
2022-09-29 00:21:43 +0300

    s

--- index.js
+++ index.js
@@ -4,18 +4,23 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+const general = io.of("/general");
+const football = io.of("/football");
+const basketball = io.of("/basketball");
+var people = {};
+
+let generalTotalUser = 0;
+let footballTotalUser = 0;
+let basketballTotalUser = 0;
+
 app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
-const general = io.of("/general");
-var people = {};
 
-var basketballTotalUser = 0;
+let numUsers = 0;
+
 
 general.on('connection', function (socket) {
 
@@ -43,8 +48,117 @@ general.on('connection', function (socket) {
     });
 });
 
+football.on('connection', function (socket) {
 
-const PORT = process.env.PORT || 5000; 
-server.listen(PORT, ()=>{
-    console.log(`Server started on port ${PORT}`); 
+    nickname = socket.handshake.query['nickname'];
+    people[socket.id] = nickname;
+
+    socket.on('join', function(msg){
+        footballTotalUser = footballTotalUser + 1;
+        console.log(nickname + ": has joined to general channel");
+        console.log("channel user count:" + footballTotalUser);
+        socket.broadcast.emit('join', {nickname: nickname, count: footballTotalUser});
+        socket.emit('activeUser', {count: footballTotalUser});
+    });
+
+    socket.on('disconnect', function(msg){
+        footballTotalUser = footballTotalUser - 1;
+        console.log( people[socket.id] + ": has left to general channel");
+        console.log("channel user count:" + footballTotalUser);
+        socket.broadcast.emit('left', {nickname:  people[socket.id], count: footballTotalUser});
+    });
+
+    socket.on('new_message', function(msg){
+        console.log(msg.nickname + " has send message: " + msg.message);
+        socket.broadcast.emit('new_message', {nickname: msg.nickname, message: msg.message});
+    });
+});
+
+basketball.on('connection', function (socket) {
+
+    nickname = socket.handshake.query['nickname'];
+    people[socket.id] = nickname;
+
+    socket.on('join', function(msg){
+        basketballTotalUser = basketballTotalUser + 1;
+        console.log(nickname + ": has joined to general channel");
+        console.log("channel user count:" + basketballTotalUser);
+        socket.broadcast.emit('join', {nickname: nickname, count: basketballTotalUser});
+        socket.emit('activeUser', {count: basketballTotalUser});
+    });
+
+    socket.on('disconnect', function(msg){
+        basketballTotalUser = basketballTotalUser - 1;
+        console.log( people[socket.id] + ": has left to general channel");
+        console.log("channel user count:" + basketballTotalUser);
+        socket.broadcast.emit('left', {nickname:  people[socket.id], count: basketballTotalUser});
+    });
+
+    socket.on('new_message', function(msg){
+        console.log(msg.nickname + " has send message: " + msg.message);
+        socket.broadcast.emit('new_message', {nickname: msg.nickname, message: msg.message});
+    });
+});
+
+io.on('connection', (socket) => {
+    let addedUser = false;
+
+    // when the client emits 'new message', this listens and executes
+    socket.on('new message', (data) => {
+        // we tell the client to execute 'new message'
+        socket.broadcast.emit('new message', {
+          username: socket.username,
+          message: data
+        });
+    });
+    //console.log(socket.id, "a user connected to server!");
+
+    // when the client emits 'add user', this listens and executes
+    socket.on('add user', (username) => {
+        if (addedUser) return;
+
+        // we store the username in the socket session for this client
+        socket.username = username;
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', {
+          numUsers: numUsers
+        });
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('user joined', {
+          username: socket.username,
+          numUsers: numUsers
+        });
+    });
+
+    // when the client emits 'typing', we broadcast it to others
+      socket.on('typing', () => {
+        socket.broadcast.emit('typing', {
+          username: socket.username
+        });
+      });
+
+      // when the client emits 'stop typing', we broadcast it to others
+      socket.on('stop typing', () => {
+        socket.broadcast.emit('stop typing', {
+          username: socket.username
+        });
+      });
+
+    socket.on('disconnect', function () {
+        if (addedUser){
+            --numUsers;
+
+            // echo globally that this client has left
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
+    });
+});
+
+server.listen(app.get('port'), function(){
+    console.log("Server is now running...");
+    console.log("Port is on", app.get('port'))
 });
