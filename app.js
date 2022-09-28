676beaf3b655abbc835a06145a4a46875858877e
Abbas Aleinzy | عباس العنزي <42795041+almgro7al3nzy@users.noreply.github.com>
2022-09-28 23:59:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 const general = io.of("/general");
-const football = io.of("/football");
-const basketball = io.of("/basketball");
 var people = {};
 
 var generalTotalUser = 0;
@@ -29,58 +27,6 @@ general.on('connection', function (socket) {
         console.log( people[socket.id] + ": has left to general channel");
         console.log("channel user count:" + generalTotalUser);
         socket.broadcast.emit('left', {nickname:  people[socket.id], count: generalTotalUser});
-    });
-
-    socket.on('new_message', function(msg){
-        console.log(msg.nickname + " has send message: " + msg.message);
-        socket.broadcast.emit('new_message', {nickname: msg.nickname, message: msg.message});
-    });
-});
-
-football.on('connection', function (socket) {
-
-    nickname = socket.handshake.query['nickname'];
-    people[socket.id] = nickname;
-
-    socket.on('join', function(msg){
-        footballTotalUser = footballTotalUser + 1;
-        console.log(nickname + ": has joined to general channel");
-        console.log("channel user count:" + footballTotalUser);
-        socket.broadcast.emit('join', {nickname: nickname, count: footballTotalUser});
-        socket.emit('activeUser', {count: footballTotalUser});
-    });
-
-    socket.on('disconnect', function(msg){
-        footballTotalUser = footballTotalUser - 1;
-        console.log( people[socket.id] + ": has left to general channel");
-        console.log("channel user count:" + footballTotalUser);
-        socket.broadcast.emit('left', {nickname:  people[socket.id], count: footballTotalUser});
-    });
-
-    socket.on('new_message', function(msg){
-        console.log(msg.nickname + " has send message: " + msg.message);
-        socket.broadcast.emit('new_message', {nickname: msg.nickname, message: msg.message});
-    });
-});
-
-basketball.on('connection', function (socket) {
-
-    nickname = socket.handshake.query['nickname'];
-    people[socket.id] = nickname;
-
-    socket.on('join', function(msg){
-        basketballTotalUser = basketballTotalUser + 1;
-        console.log(nickname + ": has joined to general channel");
-        console.log("channel user count:" + basketballTotalUser);
-        socket.broadcast.emit('join', {nickname: nickname, count: basketballTotalUser});
-        socket.emit('activeUser', {count: basketballTotalUser});
-    });
-
-    socket.on('disconnect', function(msg){
-        basketballTotalUser = basketballTotalUser - 1;
-        console.log( people[socket.id] + ": has left to general channel");
-        console.log("channel user count:" + basketballTotalUser);
-        socket.broadcast.emit('left', {nickname:  people[socket.id], count: basketballTotalUser});
     });
 
     socket.on('new_message', function(msg){
