1b3d089334e927f55304fadaed060a302f2ed497
Abbas Aleinzy | عباس العنزي <42795041+almgro7al3nzy@users.noreply.github.com>
2022-09-29 12:12:39 +0300

    00

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
-import React from "react"; 
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import { useRoutes } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core';
+import GlobalStyles from 'src/components/GlobalStyles';
+import 'src/mixins/chartjs';
+import theme from 'src/theme';
+import routes from 'src/routes';
 
-import { BrowserRouter as Router, Route } from "react-router-dom"; 
-
-import Join from "./components/Join/Join";
-import Chat from "./components/Chat/Chat"; 
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Join from "./components/Join/Join";
+// import Chat from "./components/Chat/Chat";
 import "./main.css"
 
-import { positions, Provider } from "react-alert";
-import AlertTemplate from "react-alert-template-basic";
+const App = () => {
+  const routing = useRoutes(routes);
 
-
-const options = {
-    timeout: 5000,
-    position: positions.MIDDLE
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      {routing}
+    </ThemeProvider>
+  );
 };
 
-const App = () => { 
-    
-    return ( 
-        <Provider template={AlertTemplate} {...options}>
-            <Router> 
-                <Route path="/" exact component={Join} />
-                <Route path="/join" exact component={Join} />
-                <Route path="/chat" exact component={Chat} /> 
-            </Router>
-        </Provider>
-    ); 
-}
-
-export default App; 
-
-/* User enters data in "/", that data is passed to "/chat" using query-strings" */ 
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom"; 
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
+import App from './App';
 
-import App from "./App";
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'));
 
-ReactDOM.render(<App/>,document.querySelector("#root"));
-
+serviceWorker.unregister();
